a17ba1ba24f0a43977ede2adb832a574c254a3dc
Orbay Ay <165911802+orbayay@users.noreply.github.com>
2024-05-06 17:17:46 +0300

    Add files via upload

---
+++ Extra/Extra/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fault Managment - Try Catch
+
+            try
+            {
+                Console.WriteLine("Please enter a int value : ");
+                int n1 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Please enter a int value : ");
+                int n2 = Convert.ToInt16(Console.ReadLine());
+                int multiple1 = n1 * n2;
+                Console.WriteLine("Result : " + multiple1);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mistakeeee!!!!!!!!");
+            }
+            finally
+            {
+                Console.WriteLine("This place is worked :)");
+            }
+
+            Console.Read();
+        }
+    }
+}
