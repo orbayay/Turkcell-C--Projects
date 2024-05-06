e45acd9f6413cb9db650ba300558b052f0c72ed1
Orbay Ay <165911802+orbayay@users.noreply.github.com>
2024-05-06 17:19:20 +0300

    Add files via upload

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Captcha Example
+            int n1, n2, n3, n4, n5;
+            Random rand1 = new Random();
+            n1 = rand1.Next(0, 10);
+            n2 = rand1.Next(0, 10);
+            n3 = rand1.Next(0, 10);
+            n4 = rand1.Next(0, 10);
+            n5 = rand1.Next(0, 10);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            string[] character = { "a", "A", "b", "B", "c", "C", "d", "D", "e", "E" };
+            Console.WriteLine(n1 + character[n2] + n3 + character[n4]);
+            Console.Read();
+        }
+    }
+}
---
+++ If_Else/If_Else/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String Değişkenler ile Switch-Case Uygulaması");
+            Console.WriteLine();
+            string mevsim;
+            Console.Write("Please enter a season in Turkish: ");
+            mevsim = Console.ReadLine();
+            switch (mevsim)
+            {
+                case "yaz":Console.WriteLine("Haziran - Temmuz - Ağustos"); break;
+                case "kış":Console.WriteLine("Aralık-Ocak-Şubat");break;
+                case "ilkbahar":Console.WriteLine("Mart-Nisan-Mayıs");break;
+                case "sonbahar":Console.WriteLine("Eylül-Ekim-Kasım");break;
+                default:Console.WriteLine("Wrong Entry Dude");break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
