555edd6cd379e437020c5b586aa34c1b1d7dd264
Orbay Ay <165911802+orbayay@users.noreply.github.com>
2024-05-06 17:17:18 +0300

    Add files via upload

---
+++ DiceRollGame/DiceRollGame/Program.cs
@@ -0,0 +1,95 @@
+﻿using DiceRollGame.Game;
+using DiceRollGame.UserCommunication;
+
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
+GuessingGame.PrintResult(gameResult);
+
+Console.ReadKey();
+
+namespace DiceRollGame.UserCommunication
+{
+    public static class ConsoleReader
+    {
+        public static int ReadInteger(string message)
+        {
+            int result;
+            do
+            {
+                Console.WriteLine(message);
+            }
+            while (!int.TryParse(Console.ReadLine(), out result));
+            return result;
+        }
+    }
+}
+namespace DiceRollGame.Game
+{
+    public class Dice
+    {
+        private readonly Random _random;
+        private const int SidesCount = 6;
+
+        public Dice(Random random)
+        {
+            _random = random;
+        }
+
+        public int Roll() => _random.Next(1, SidesCount + 1);
+    }
+}
+namespace DiceRollGame.Game
+{
+    public enum GameResult
+    {
+        Victory,
+        Loss
+    }
+}
+using DiceRollGame.UserCommunication;
+
+namespace DiceRollGame.Game
+{
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+        }
+
+        public GameResult Play()
+        {
+            var diceRollResult = _dice.Roll();
+            Console.WriteLine(
+                $"Dice rolled. Guess what number it shows in {InitialTries} tries.");
+
+            var triesLeft = InitialTries;
+            while (triesLeft > 0)
+            {
+                var guess = ConsoleReader.ReadInteger("Enter a number:");
+                if (guess == diceRollResult)
+                {
+                    return GameResult.Victory;
+                }
+                Console.WriteLine("Wrong number.");
+                --triesLeft;
+            }
+            return GameResult.Loss;
+        }
+
+        public static void PrintResult(GameResult gameResult)
+        {
+            string message = gameResult == GameResult.Victory
+                ? "You win!"
+                : "You lose :(";
+
+            Console.WriteLine(message);
+        }
+    }
+}
