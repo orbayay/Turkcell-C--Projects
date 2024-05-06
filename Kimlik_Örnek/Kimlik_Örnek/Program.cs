f793fe895622ca7f37b4a4680f16e0406b0306dc
Orbay Ay <165911802+orbayay@users.noreply.github.com>
2024-05-06 17:20:23 +0300

    Add files via upload

---
+++ Kimlik_Örnek/Kimlik_Örnek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kimlik_Örnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Kimlik Bilgileri *****");
+            Console.WriteLine();
+            Console.WriteLine("Orbay");
+            Console.WriteLine("Ay");
+            Console.WriteLine("Istanbul Aydin University");
+            Console.Write("Istanbul");
+            Console.Read();
+        }
+    }
+}
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            do
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+            while (number <= 5);
+            Console.Read();
+        }
+    }
+}
